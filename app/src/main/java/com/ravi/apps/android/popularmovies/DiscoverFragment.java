387c05932c89c8e80f20e8d73a148ef4cb2e8b6a
Ravi <ravi10010101@gmail.com>
2015-08-28 21:18:11 +0530

    feat: Create movie discovery related layouts for phones

---
+++ app/src/main/java/com/ravi/apps/android/popularmovies/DiscoverFragment.java
@@ -0,0 +1,22 @@
+package com.ravi.apps.android.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DiscoverFragment extends Fragment {
+
+    public DiscoverFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_discover, container, false);
+    }
+}
